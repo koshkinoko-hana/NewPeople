c5d3d9177fb3f5e98ed0d84384dcd027da01be5f
Olga <o2-2010@mail.ru>
2020-12-08 23:29:35 +0300

    Добавлено поле status

---
+++ db/migrations/V002__create_requests_table.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS requests;
+CREATE TABLE requests (
+                          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                          age INT NOT NULL,
+                          phone VARCHAR(11) NOT NULL,
+                          attendance ENUM('FULL_TIME', 'PART_TIME') NOT NULL,
+                          name VARCHAR(255),
+                          status ENUM('PROCESSED', 'UNPROCESSED') NOT NULL,
+                          comment VARCHAR(255)
+);
