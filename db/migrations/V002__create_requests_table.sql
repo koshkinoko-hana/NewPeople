6f3e55d4027c68c604e2f3f689fe3de9bf08a824
Olga <o2-2010@mail.ru>
2020-12-09 08:52:57 +0300

    изменение скрипта миграции

--- db/migrations/V002__create_requests_table.sql
+++ db/migrations/V002__create_requests_table.sql
@@ -1,10 +1,2 @@
-DROP TABLE IF EXISTS requests;
-CREATE TABLE requests (
-                          id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                          age INT NOT NULL,
-                          phone VARCHAR(11) NOT NULL,
-                          attendance ENUM('FULL_TIME', 'PART_TIME') NOT NULL,
-                          name VARCHAR(255),
-                          status_claim ENUM('PROCESSED', 'UNPROCESSED') NOT NULL,
-                          comment VARCHAR(255)
-);
+alter table requests add column
+    status_claim ENUM('PROCESSED', 'UNPROCESSED') NOT NULL default 'UNPROCESSED';
